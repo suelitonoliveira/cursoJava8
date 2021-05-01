7f9efff31c38737b9d19325e7028d50b30cf8c23
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-01 13:43:15 -0300

    Controle de Fluxo com Switch

---
+++ src/cursoJava8/capitulo2/ControleFluxoSwitch.java
@@ -0,0 +1,57 @@
+package cursoJava8.capitulo2;
+
+public class ControleFluxoSwitch {
+
+	public static void main(String[] args) {
+
+		byte dia = 5;
+		// char, int , byte, short, enum e String pode ir dentro do switch
+		switch (dia) {
+		case 1:
+
+			System.out.println("Domingo");
+			break;
+		case 2:
+
+			System.out.println("Segunda");
+			break;
+		case 3:
+
+			System.out.println("Terça");
+			break;
+		case 4:
+
+			System.out.println("Quarta");
+			break;
+
+		case 5:
+
+			System.out.println("Quinta");
+
+			break; // quando encontra sai do bloco de código
+
+		case 6:
+
+			System.out.println("Sexta");
+			break;
+
+		case 7:
+
+			System.out.println("Sabado");
+			break;
+
+		}
+		
+		char sexo = 'M';
+		switch (sexo) {
+		case 'F':
+			System.out.println("feminino");
+			break;
+		case 'M':
+			System.out.println("Masculino");
+		default:
+			break;
+		}
+
+	}
+}
