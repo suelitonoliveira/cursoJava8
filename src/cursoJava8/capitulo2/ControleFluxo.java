ecbeff175080dba12b772e4d68cd33c22c4f7b50
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-05-01 13:26:45 -0300

    controle de fluxo

---
+++ src/cursoJava8/capitulo2/ControleFluxo.java
@@ -0,0 +1,18 @@
+package cursoJava8.capitulo2;
+
+public class ControleFluxo {
+
+	public static void main(String[] args) {
+		
+		int idade = 20;
+		String status;
+		/*
+		 * if (idade < 10) { status = "Não adulto"; }else { status = "Adulto"; }
+		 */
+		//operador ternario
+		status = idade < 10 ? "Não adulto" : "Adulto";
+		System.out.println(status);
+
+	}
+
+}
